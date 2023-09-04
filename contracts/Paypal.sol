baf960855675ba7c5d22ead9304946c90ea60a5a
Honeybadger <rajimoshood200482@gmail.com>
2023-09-04 13:48:07 +0100

    my first commit

---
+++ contracts/Paypal.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+contract Paypal {
+
+    // Define the owner of the smart contract
+
+    address public owner;
+
+
+    constructor(){
+        owner = msg.sender;
+    }
+
+    
+    // create Struct and mapping for request, transaction & more 
+    
+
+    //Add a name to wallet address
+
+
+    //Create a request 
+
+
+    //Pay a request
+
+
+    //Get all request sent to a user 
+
+
+    //Get all historical transactions user has been a part of 
+}
